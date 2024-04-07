795e9af17e4a7749d8654a0b053b83d130b0e34f
jungian6 <122241019+jungian6@users.noreply.github.com>
2024-04-07 21:59:54 +0100

    Refactor

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import math
 from errors import *
 from nodes import *
 from symbol_table import SymbolTable
+from list import List
+from Values import Value
 
 DIGITS = '0123456789'
 LETTERS = string.ascii_letters
@@ -1114,83 +1116,6 @@ class RTResult:
         )
 
 
-class Value:
-    """Value Class"""
-
-    def __init__(self):
-        self.set_pos()
-        self.set_context()
-
-    def set_pos(self, pos_start=None, pos_end=None):
-        """Set the position of the value"""
-        self.pos_start = pos_start
-        self.pos_end = pos_end
-        return self
-
-    def set_context(self, context=None):
-        """Set the context of the value"""
-        self.context = context
-        return self
-
-    def added_to(self, other):
-        return None, self.illegal_operation(other)
-
-    def subbed_by(self, other):
-        return None, self.illegal_operation(other)
-
-    def multed_by(self, other):
-        return None, self.illegal_operation(other)
-
-    def dived_by(self, other):
-        return None, self.illegal_operation(other)
-
-    def power_by(self, other):
-        return None, self.illegal_operation(other)
-
-    def get_comparison_eq(self, other):
-        return None, self.illegal_operation(other)
-
-    def get_comparison_ne(self, other):
-        return None, self.illegal_operation(other)
-
-    def get_comparison_lt(self, other):
-        return None, self.illegal_operation(other)
-
-    def get_comparison_gt(self, other):
-        return None, self.illegal_operation(other)
-
-    def get_comparison_lte(self, other):
-        return None, self.illegal_operation(other)
-
-    def get_comparison_gte(self, other):
-        return None, self.illegal_operation(other)
-
-    def anded_by(self, other):
-        return None, self.illegal_operation(other)
-
-    def ored_by(self, other):
-        return None, self.illegal_operation(other)
-
-    def notted(self, other):
-        return None, self.illegal_operation(other)
-
-    def execute(self, args):
-        return RTResult().failure(self.illegal_operation())
-
-    def copy(self):
-        raise Exception('No copy method defined')
-
-    def is_true(self):
-        return False
-
-    def illegal_operation(self, other=None):
-        if not other:
-            other = self
-        return RTError(
-            self.pos_start, other.pos_end,
-            'Illegal operation',
-            self.context
-        )
 
 
 class Number(Value):
@@ -1415,68 +1340,6 @@ class String(Value):
 
     def __repr__(self):
         return f'"{self.value}"'
-
-
-class List(Value):
-    """List of elements"""
-
-    def __init__(self, elements):
-        super().__init__()
-        self.elements = elements
-
-    def added_to(self, other):
-        """Concatenate a list with another list or a number"""
-        new_list = self.copy()
-        new_list.elements.append(other)
-        return new_list, None
-
-    def subbed_by(self, other):
-        if isinstance(other, Number):
-            new_list = self.copy()
-            try:
-                new_list.elements.pop(other.value)
-                return new_list, None
-            except:
-                return None, RTError(
-                    other.pos_start, other.pos_end,
-                    'Element at this index could not be removed from list because index is out of bounds',
-                    self.context
-                )
-        else:
-            return None, Value.illegal_operation(self, other)
-
-    def multed_by(self, other):
-        if isinstance(other, List):
-            new_list = self.copy()
-            new_list.elements.extend(other.elements)
-            return new_list, None
-        else:
-            return None, Value.illegal_operation(self, other)
-
-    def dived_by(self, other):
-        if isinstance(other, Number):
-            try:
-                return self.elements[other.value], None
-            except:
-                return None, RTError(
-                    other.pos_start, other.pos_end,
-                    'Element at this index could not be retrieved from list because index is out of bounds',
-                    self.context
-                )
-        else:
-            return None, Value.illegal_operation(self, other)
-
-    def copy(self):
-        copy = List(self.elements)
-        copy.set_pos(self.pos_start, self.pos_end)
-        copy.set_context(self.context)
-        return copy
-
-    def __str__(self):
-        return ", ".join([str(x) for x in self.elements])
-
-    def __repr__(self):
-        return f'[{", ".join([repr(x) for x in self.elements])}]'
 
 
 class BaseFunction(Value):
